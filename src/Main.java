25bc022520b9ba9bf45628093ad495dfe9910209
Ryan Bezerra <ryandonascimentobezerra@gmail.com>
2024-03-13 22:57:10 -0300

    Versão 0.4

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import javax.imageio.ImageIO;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
@@ -13,43 +15,66 @@ public class Main {
             BufferedImage image = ImageIO.read(file);
 
             // Aplicar alterações na imagem original
-            alterarImagemOriginal(image);
+            BufferedImage firstModifiedImage = alterarImagemOriginal(image);
 
-            // Salvar a imagem alterada como "modificado1.png"
-            salvarImagem(image, "Imagens\\modificado1.png");
+            // Salvar a imagem alterada como "modificado.png"
+            salvarImagem(firstModifiedImage, "Imagens\\modificado.png");
 
             // Converter a imagem para escala de cinza
             BufferedImage greyscaleImage = converterParaEscalaDeCinza(image);
 
-            // Salvar a imagem em escala de cinza como "modificado2.png"
-            salvarImagem(greyscaleImage, "Imagens\\modificado2.png");
+            // Salvar a imagem em escala de cinza como "modificado_greyscale.png"
+            salvarImagem(greyscaleImage, "Imagens\\modificado_greyscale.png");
 
-            // Exibir o RGB de cada pixel na imagem original
-            exibirRGB(image);
+            // Salvar a imagem em escala de vermelho como "modificado_red.png"
+            BufferedImage redScaleImage = converterParaEscalaColorida(image, 'r');
+            salvarImagem(redScaleImage, "Imagens\\modificado_red.png");
+
+            // Salvar a imagem em escala de verde como "modificado_green.png"
+            BufferedImage greenScaleImage = converterParaEscalaColorida(image, 'g');
+            salvarImagem(greenScaleImage, "Imagens\\modificado_green.png");
+
+            // Salvar a imagem em escala de azul como "modificado_blue.png"
+            BufferedImage blueScaleImage = converterParaEscalaColorida(image, 'b');
+            salvarImagem(blueScaleImage, "Imagens\\modificado_blue.png");
+
+            // Exibir o RGB de cada pixel na primeira imagem modificada
+            exibirRGB(firstModifiedImage);
 
             System.out.println("Imagens salvas com sucesso!");
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // Função para aplicar alterações na imagem original
-    public static void alterarImagemOriginal(BufferedImage image) {
+    public static BufferedImage alterarImagemOriginal(BufferedImage image) {
         int largura = image.getWidth();
         int altura = image.getHeight();
 
+        // Criar uma cópia da imagem original
+        BufferedImage modifiedImage = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < largura; x++) {
+                modifiedImage.setRGB(x, y, image.getRGB(x, y));
+            }
+        }
+
         // Pintar o primeiro pixel de azul
-        image.setRGB(0, 0, Color.BLUE.getRGB());
+        modifiedImage.setRGB(0, 0, 0x0000FF);
 
         // Pintar o pixel central de verde
         int centroX = largura / 2;
         int centroY = altura / 2;
-        image.setRGB(centroX, centroY, Color.GREEN.getRGB());
+        modifiedImage.setRGB(centroX, centroY, 0x00FF00);
 
         // Pintar o último pixel de vermelho
         int ultimoX = largura - 1;
         int ultimoY = altura - 1;
-        image.setRGB(ultimoX, ultimoY, Color.RED.getRGB());
+        modifiedImage.setRGB(ultimoX, ultimoY, 0xFF0000);
+
+        return modifiedImage;
     }
 
     // Função para converter a imagem para escala de cinza
@@ -79,16 +104,12 @@ public class Main {
         return greyscaleImage;
     }
 
-    // Função para salvar a imagem
-    public static void salvarImagem(BufferedImage image, String filePath) throws IOException {
-        File outputFile = new File(filePath);
-        ImageIO.write(image, "png", outputFile);
-    }
-
-    // Função para exibir o RGB de cada pixel na imagem original
-    public static void exibirRGB(BufferedImage image) {
+    // Função para converter a imagem para escala de cor especificada
+    public static BufferedImage converterParaEscalaColorida(BufferedImage image, char color) {
         int largura = image.getWidth();
         int altura = image.getHeight();
+
+        BufferedImage colorScaleImage = new BufferedImage(largura, altura, BufferedImage.TYPE_INT_RGB);
 
         for (int y = 0; y < altura; y++) {
             for (int x = 0; x < largura; x++) {
@@ -96,8 +117,57 @@ public class Main {
                 int red = (rgb >> 16) & 0xFF;
                 int green = (rgb >> 8) & 0xFF;
                 int blue = rgb & 0xFF;
+
+                // Ajustar os valores de acordo com a cor especificada
+                switch (color) {
+                    case 'r':
+                        red = red;
+                        green = 0;
+                        blue = 0;
+                        break;
+                    case 'g':
+                        red = 0;
+                        green = green;
+                        blue = 0;
+                        break;
+                    case 'b':
+                        red = 0;
+                        green = 0;
+                        blue = blue;
+                        break;
+                    default:
+                        break;
+                }
+
+                int colorValue = (red << 16) | (green << 8) | blue;
+
+                colorScaleImage.setRGB(x, y, colorValue);
+            }
+        }
+
+        return colorScaleImage;
+    }
+
+    // Função para salvar a imagem
+    public static void salvarImagem(BufferedImage image, String filePath) throws IOException {
+        File outputFile = new File(filePath);
+        ImageIO.write(image, "png", outputFile);
+    }
+
+    // Função para exibir o RGB de cada pixel na imagem
+    public static void exibirRGB(BufferedImage firstModifiedImage) {
+        int largura = firstModifiedImage.getWidth();
+        int altura = firstModifiedImage.getHeight();
+
+        for (int y = 0; y < altura; y++) {
+            for (int x = 0; x < largura; x++) {
+                int rgb = firstModifiedImage.getRGB(x, y);
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = rgb & 0xFF;
                 System.out.println("Pixel (" + x + ", " + y + "): RGB = (" + red + ", " + green + ", " + blue + ")");
             }
         }
     }
+
 }
